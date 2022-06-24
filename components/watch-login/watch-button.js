52168f4e8f81534d9395792ea6889835a6d7b1d3
1046453231 <108121976+1046453231@users.noreply.github.com>
2022-06-24 17:03:48 +0800

    Add files via upload

---
+++ components/watch-login/watch-button.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/watch-login/watch-button"],{"2e22":function(t,n,e){"use strict";var r;e.d(n,"b",(function(){return u})),e.d(n,"c",(function(){return a})),e.d(n,"a",(function(){return r}));var u=function(){var t=this,n=t.$createElement;t._self._c},a=[]},4251:function(t,n,e){},"507f":function(t,n,e){"use strict";e.r(n);var r=e("fb14"),u=e.n(r);for(var a in r)"default"!==a&&function(t){e.d(n,t,(function(){return r[t]}))}(a);n["default"]=u.a},"93fb":function(t,n,e){"use strict";e.r(n);var r=e("2e22"),u=e("507f");for(var a in u)"default"!==a&&function(t){e.d(n,t,(function(){return u[t]}))}(a);e("d76d");var o,f=e("f0c5"),i=Object(f["a"])(u["default"],r["b"],r["c"],!1,null,null,null,!1,r["a"],o);n["default"]=i.exports},d76d:function(t,n,e){"use strict";var r=e("4251"),u=e.n(r);u.a},fb14:function(t,n,e){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var r={props:{text:String,rotate:{type:[Boolean,String],default:!1},bgColor:{type:String,default:"linear-gradient(to right, rgba(0,0,0,0.7), rgba(0,0,0,0.6))"},fontColor:{type:String,default:"#FFFFFF"}},computed:{_rotate:function(){return"false"!==String(this.rotate)}}};n.default=r}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/watch-login/watch-button-create-component',
+    {
+        'components/watch-login/watch-button-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("93fb"))
+        })
+    },
+    [['components/watch-login/watch-button-create-component']]
+]);
